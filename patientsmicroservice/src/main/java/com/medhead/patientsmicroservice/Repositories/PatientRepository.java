70ec4be6b173037504ab105a553d1f8a101f42ed
ludo77100 <ludovic.s@protonmail.com>
2024-07-30 15:01:49 +0200

    Correction d'un bug au lancement de l'application

--- patientsmicroservice/src/main/java/com/medhead/patientsmicroservice/Repositories/PatientRepository.java
+++ patientsmicroservice/src/main/java/com/medhead/patientsmicroservice/Repositories/PatientRepository.java
@@ -12,5 +12,9 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Optional<Patient> findPatientByFirstNameAndLastNameIgnoreCase(String firstName, String lastName);
 
-    @Query("SELECT p FROM Patient p WHERE :idCardNumber IN elements(p.idCardNumberList)")
-    Optional<Patient> findPatientByIdCardNumber(String idCardNumber);}
+    @Query("SELECT p FROM Patient p JOIN p.idCardNumberList id WHERE id = :idCardNumber")
+    Optional<Patient> findPatientByIdCardNumber(String idCardNumber);
+
+}
+
+
